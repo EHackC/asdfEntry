ae69ec66d01881d5fa787bb2bad67eeb3443e21e
EHackC <lolisfun0987@gmail.com>
2022-12-25 16:56:32 +0900

    주석 추가

--- src/screen.cpp
+++ src/screen.cpp
@@ -3,12 +3,12 @@
 
 //화면에 그리는 함수
 void Screen::printScreen(int width,int height){
-    int screenSize=min(scrollY+height,static_cast<int>(content.size()));
+    int screenSize=min(scrollY+height,static_cast<int>(content.size())); //출력할 길이 얻기
     for(int i=scrollY;i<screenSize;i++){
-        gotoxy(left,top+i-scrollY);
+        gotoxy(left,top+i-scrollY); //출력할 줄로 이동
         string buff=content.at(i);
-        if(buff.length()>left){
-            cout<<buff.substr(scrollX,min(width,static_cast<int>(buff.length())-scrollX))<<endl;
+        if(buff.length()>left){ //만약 화면에 보이면
+            cout<<buff.substr(scrollX,min(width,static_cast<int>(buff.length())-scrollX))<<endl; //그 줄 출력
         }
     }
 }
